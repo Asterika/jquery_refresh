997188baa95bfb4d879bea1f7db2892f88b36175
Krystal Williams <40532253+Asterika@users.noreply.github.com>
2019-03-06 12:01:56 -0800

    Complete DOM traversing

--- js/traversing.js
+++ js/traversing.js
@@ -22,12 +22,35 @@
 // })
 
 //MOVING TO THE NEXT ELEMENT IN THE RESULTS YOU'VE FOUND USING CHILDREN OR FIND
-$(document).ready(function() {
-  //first(), last()
-  //when you know you want the first children
-  var result = $('#animals').children().first();
-  console.log(result);
+// $(document).ready(function() {
+//   //first(), last()
+//   //when you know you want the first children
+//   var result = $('#animals').children().first().children('.creature').last();
+//   console.log(result);
+//
+//   //prev() - want all children dog, cat, forest, prairie
+//   //in this case, remove class selector
+//   //first .prev takes us to Forest
+//   //second takes us to cats
+//   result = $('#animals').children().first().children().last().prev().prev();
+//   console.log(result);
+//
+//   //next() - longer way in this case
+//   result = $('#animals').children().first().children('.creature').first().next();
+//   console.log(result);
+// })
 
-
-
-})
+//MOVING UP THE CHAIN TO PARENT ELEMENT
+// $(document).ready(function() {
+//   //parent()
+//   var result = $('#cat').parent(); //=> mammals
+//   //parents()
+//   console.log(result);
+//
+//   result = $('#cat').parents(); //=> mammals, animals, body and html tag of page
+//   console.log(result);
+//
+//   //closest() - when you may not know exactly where it inspect
+//   result = $('#cat').closest('.category');
+//   console.log(result);
+// })
